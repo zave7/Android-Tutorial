dd3a2466d2ebeb1910421f4bfce8bca590360109
zave7 <zave7@naver.com>
2021-05-26 10:15:48 +0900

    메뉴 ui 작업중

--- app/src/main/java/com/example/tutorial/ui/activity/MenuActivity.kt
+++ app/src/main/java/com/example/tutorial/ui/activity/MenuActivity.kt
@@ -2,9 +2,14 @@ package com.example.tutorial.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 import com.example.tutorial.R
 
 class MenuActivity : AppCompatActivity() {
+
+    /*val recyclerView : RecyclerView by lazy {
+    }*/
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
